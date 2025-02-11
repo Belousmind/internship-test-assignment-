3d85d70c6521e880d50e813b19b08807628fe217
belousmind <belousmind@gmail.com>
2025-02-11 17:43:40 +0300

    feat: add hook to List element that request posts

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import ListItem from '../ListItem/ListItem';
 
 const List = () => {
+
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/items")
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Ошибка загрузки данных:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  const elements = items.map((item) => (
+    <ListItem key={item.id} item={item} />
+  ))
   
   return (  
   <>
   <h1>Список объявлений</h1>
     <Link to="/form">Создать объявление</Link>
     <hr />
-    <span>Нет объявлений</span>
+
+    {loading ? <p>Загрузка...</p> : null }
+    {items.length === 0 ? <p>Объявления не найдены</p> : elements}
+ 
   </>  
 )};
 

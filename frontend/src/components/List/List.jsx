54b219ab3a35179dcc6e5be0932708faba3724e6
belousmind <belousmind@gmail.com>
2025-02-12 12:07:57 +0300

    feat: add pagination to list page

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import ListItem from '../ListItem/ListItem';
+import { Pagination } from "antd";
 
 const List = () => {
 
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 5;
 
   useEffect(() => {
     fetch("http://localhost:3000/items")
@@ -21,19 +25,30 @@ const List = () => {
       });
   }, []);
 
-  const elements = items.map((item) => (
-    <ListItem key={item.id} item={item} />
-  ))
+  const indexOfLastItem = currentPage * pageSize;
+  const indexOfFirstItem = indexOfLastItem - pageSize;
+  const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
   
   return (  
   <>
-  <h1>Список объявлений</h1>
+    <h1>Список объявлений</h1>
     <Link to="/form">Создать объявление</Link>
     <hr />
 
-    {loading ? <p>Загрузка...</p> : null }
-    {items.length === 0 ? <p>Объявления не найдены</p> : elements}
- 
+    {loading && <p>Загрузка...</p>}
+    {items.length === 0 && !loading ? <p>Объявления не найдены</p> : null}
+
+    {currentItems.map((item) => (
+      <ListItem key={item.id} item={item} />
+    ))}
+
+    <Pagination
+      current={currentPage}
+      pageSize={pageSize}
+      total={items.length}
+      onChange={(page) => setCurrentPage(page)}
+      style={{ marginTop: "20px", textAlign: "center" }}
+    />
   </>  
 )};
 
